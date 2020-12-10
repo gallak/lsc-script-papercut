33811103a519d0adb1a009685c92e40a08818e39
Antoine Gallavardin <antoine.gallavardin@free.fr>
2020-12-10 22:59:57 +0100

    take care of CamelCase in cfg file, correct list_user output

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ from lib import papercut
 
 # import de la configuration
 confParser = configparser.ConfigParser()
+confParser.optionxform = lambda option: option
 confParser.read("papercut.cfg")
 
 # creation du logger
@@ -30,13 +31,12 @@ logger.addHandler(fh)
 
 # option from commandline
 
-cliParser=ArgumentParser( prog = 'run', description =""" Script used to sync PAPERCUT """)
-
+cliParser=ArgumentParser( prog = 'run', description =""" Script used to sync PAPERCUT with LSC trough lsc plugin exec""")
 cliParser.add_argument("--action", help="one of the following action : getAllUser / getOneUser (need username) / createOneUser (need username and ldif) / updateOneUser (need username and ldif from stdin) / delOneUser (need username) / dumpAllUser")
 cliParser.add_argument("--user", help="username to use for get/updateOne/delete user from PAPERCUT server", type =str)
 
 
-PcAttributs=['username','username-alias','full-name','email','primary-card-number','secondary-card-number','office','card-pin','department']
+PcAttributs=['username-alias','full-name','email','primary-card-number','secondary-card-number','office','card-pin','department']
 
 
 if __name__ == '__main__':
@@ -50,7 +50,8 @@ if __name__ == '__main__':
   arguments = cliParser.parse_args()
   if arguments.action:
     if arguments.action == "getAllUser" :
-      pprint.pprint(pcCnx.list_users())
+      for account in pcCnx.list_users():
+        print(account)
     elif arguments.action == "getOneUser" :
       pcCnx.getPapercutLscExec(arguments.user,PcAttributs)
     elif arguments.action == "updateOneUser" :
