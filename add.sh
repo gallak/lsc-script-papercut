8b3855f9b8576417017a15e8cf63d6b1d3c6d868
Antoine Gallavardin <antoine.gallavardin@free.fr>
2020-12-19 22:59:29 +0100

    correct script wrapper (deletion of --action option)

--- add.sh
+++ add.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source $LSC_PC_BIN_PATH/env/bin/activate
-$LSC_PC_BIN_PATH/run.py --action createOneUser --user $1
+$LSC_PC_BIN_PATH/run.py --user $1
 exit $?
--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cat - >> /tmp/update.log
 source $LSC_PC_BIN_PATH/env/bin/activate
-$LSC_PC_BIN_PATH/run.py --action updateOneUser --user $1
+$LSC_PC_BIN_PATH/run.py  --user $1
 exit $?
